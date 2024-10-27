218f487b2d0362c8bddcc5401bf1b48c5b24fcad
Maria <masha-kem133@mail.ru>
2024-10-27 21:45:11 +0100

    Pagination and Input added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,14 @@
 import { Component } from 'react';
 import './footer.css';
+import { Pagination } from 'antd';
 
 export default class Footer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="footer">
+        {' '}
+        <Pagination defaultCurrent={1} total={50} />{' '}
+      </div>
+    );
   }
 }
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import './search-panel.css';
+import { Input } from 'antd';
 
 export default class SearchPanel extends Component {
   render() {
-    return <form></form>;
+    return <Input className="search-panel" placeholder="Type to search..."></Input>;
   }
 }
