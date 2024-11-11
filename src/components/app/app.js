6b5eb1a0e57b77f0d8c81c74f2edfd20e8411dd7
Maria <masha-kem133@mail.ru>
2024-11-11 22:47:55 +0100

    Tabs added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,8 @@ import Search from '../search';
 import Rated from '../rated';
 import MoviesService from '../../services/movies-service';
 
+import { Tabs, ConfigProvider } from 'antd';
+
 export default class App extends Component {
   MoviesService = new MoviesService();
 
@@ -13,10 +15,37 @@ export default class App extends Component {
   }
 
   render() {
+    const tabs = [
+      {
+        key: '1',
+        label: 'Search',
+        children: <Search />,
+      },
+      {
+        key: '2',
+        label: 'Rated',
+        children: <Rated />,
+      },
+    ];
+
     return (
-      <div>
-        <Search />
-        <Rated />
+      <div className="app">
+        <ConfigProvider
+          theme={{
+            components: {
+              Tabs: {
+                itemSelectedColor: '#1890FF',
+                itemColor: '#000000A6',
+                inkBarColor: '#1890FF',
+                fontFamily: "'Inter', sans-serif",
+                lineWidth: 0,
+                lineHeight: 2,
+              },
+            },
+          }}
+        >
+          <Tabs defaultActiveKey="1" items={tabs} centered></Tabs>
+        </ConfigProvider>
       </div>
     );
   }
--- src/components/rated/rated.js
+++ src/components/rated/rated.js
@@ -3,6 +3,6 @@ import './rated.css';
 
 export default class Rated extends Component {
   render() {
-    return <div></div>;
+    return <div className="rated"></div>;
   }
 }
